3b2f1141901662d206e7a3ffb37233981fa76784
Nelson Tam <nkltam2003@gmail.com>
2017-07-28 18:28:22 -0700

    Fully working Gitub image
    render large or small images on given owner and repository name

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # repolabels
+# A AWS Lambda project to generate Github repository images from repository data.
+
+[![Travis][build-badge]][build]
+[![npm package][npm-badge]][npm]
+[![Coveralls][coveralls-badge]][coveralls]
+
+[build-badge]: https://api.travis-ci.org/kktam/repolabels.svg?branch=master
+[build]: https://travis-ci.org/kktam/repolabels.svg?branch=master
+
+[npm-badge]: https://img.shields.io/npm/v/npm-package.png?style=flat-square
+[npm]: https://www.npmjs.org/package/npm-package
+
+[coveralls-badge]: https://coveralls.io/repos/github/kktam/repolabels/badge.svg?branch=master
+[coveralls]: https://coveralls.io/github/kktam/repolabels?branch=master
+
+# Demo
+A live demo of this project is located at 
+[Github Large image](https://102nw14mgd.execute-api.us-west-2.amazonaws.com/dev/claudiajs/claudia/large).
+[Github Small image](https://102nw14mgd.execute-api.us-west-2.amazonaws.com/dev/claudiajs/claudia/small).
+
+This project is based on the Example project from Claudia.js
+
+[https://claudiajs.com/tutorials/image-server.html](https://claudiajs.com/tutorials/image-server.html)
---
+++ api.js
@@ -0,0 +1,74 @@
+var API = require('claudia-api-builder');
+var TimeAgo = require('time-ago');
+var numeral = require('numeral');
+var got = require('got');
+var fs = require('fs-promise');
+var api = new API();
+var formatter = new TimeAgo();
+
+module.exports = api;
+
+/*
+ * Get Github repository graph data
+ */
+var getRepoDetails = function (owner, repo) {
+    return got('https://api.github.com/repos/' + owner + '/' + repo).then(function (response) {
+        return JSON.parse(response.body);
+    });
+};
+
+/*
+ * Format large number formatting for numerals
+ */
+var fmt = function (number) {
+    if (number > 999 && number < 100000) {
+        return '0.0a';
+    }
+    return '0a';
+}
+
+/*
+ * Format numbers for display
+ */
+var numeralFormat = function (number) {
+    return numeral(number).format(fmt(number));
+}
+
+/*
+ * Get Github Repo statistics in SVG image
+ */
+api.get('{owner}/{repo}/{template}', function (request) {
+    var owner = request.pathParams.owner;
+    var repo = request.pathParams.repo;
+    var templateName = request.pathParams.template;
+    if (!owner) {
+        return 'Please provide a valid Github owner name, the name provided is ' + request.pathParams.owner;
+    }
+    if (!repo) {
+        return 'Please provide a valid Github repository name, the name provided is ' + request.pathParams.repo;
+    }
+    if (!templateName) {
+        templateName = 'large';
+    }    
+    var template;
+    return fs.readFile('svg/' + templateName + '.svg', 'utf8').then(function (contents) {
+        template = contents;
+    }).then(function () {
+        return getRepoDetails(owner, repo);
+    }).then(function (repoDetails){
+        var displayName = repoDetails.full_name.length <= 20 ? repoDetails.full_name: repoDetails.name;
+        var replacements = {
+            name: displayName,
+            forks: numeralFormat(repoDetails.forks_count),
+            stars: numeralFormat(repoDetails.stargazers_count),
+            created: formatter.ago(repoDetails.created_at),
+            updated: formatter.ago(repoDetails.updated_at)
+        };
+        Object.keys(replacements).forEach(function (key) {
+            template = template.replace('(' + key + ')', replacements[key]);
+        });
+        return template;
+    });
+    
+    //return 'You asked for ' + request.pathParams.owner + '/' + request.pathParams.repo;
+}, { success: { contentType: 'image/svg+xml'}});
